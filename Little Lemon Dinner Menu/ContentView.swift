7f669d57bf8949f13964f295fc58ad4cfc928814
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-11-28 20:49:21 +0100

    Initial Commit

---
+++ Little Lemon Dinner Menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Lemon Dinner Menu
+//
+//  Created by R.A. on 28/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Little Lemon Dinner Menu/Little_Lemon_Dinner_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_Dinner_MenuApp.swift
+//  Little Lemon Dinner Menu
+//
+//  Created by R.A. on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_Dinner_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
