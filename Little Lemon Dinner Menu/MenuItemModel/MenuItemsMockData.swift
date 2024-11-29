5cfb7472fb0a79611d3938321ee272207f40d9d0
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-11-29 11:25:11 +0100

    1. Added logo to asset 2. Create Mock data: Passed tasks: 3, 4, 5

---
+++ Little Lemon Dinner Menu/MenuItemModel/MenuItemsMockData.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItemsMockData.swift
+//  Little Lemon Dinner Menu
+//
+//  Created by R.A. on 29/11/24.
+//
+
+import Foundation
---
+++ Little Lemon Dinner Menu/MenuItems/MenuItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemsView.swift
+//  Little Lemon Dinner Menu
+//
+//  Created by R.A. on 29/11/24.
+//
+
+import SwiftUI
+
+struct MenuItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuItemsView()
+}
