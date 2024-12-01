ab891d0fc9d0b6f64c8d02bab5c11a7a041ef592
Rashid Amanzholov <r.amanzholov@nexid.it>
2024-12-01 20:35:02 +0100

    Finalising the project

---
+++ Little Lemon Dinner Menu/Views/MenuItemDetails/MenuItemDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemDetailsView.swift
+//  Little Lemon Dinner Menu
+//
+//  Created by R.A. on 01/12/24.
+//
+
+import SwiftUI
+
+struct MenuItemDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuItemDetailsView()
+}
